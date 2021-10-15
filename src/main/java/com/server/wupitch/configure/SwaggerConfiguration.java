ef3de24b18f8b8136148b163c2646a34660a9ef5
vividswan <vividswan@naver.com>
2021-10-15 16:26:54 +0900

    Add: SecurityConfigure, SwaggerConfigure, LocalDateTime Class 추가

---
+++ src/main/java/com/server/wupitch/configure/SwaggerConfiguration.java
@@ -0,0 +1,40 @@
+package com.server.wupitch.configure;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.Errors;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Value("${site.address}")
+    private String SITE_ADDRESS;
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.server.wupitch"))
+                .paths(PathSelectors.any())
+                .build()
+                .host(SITE_ADDRESS)
+                .ignoredParameterTypes(AuthenticationPrincipal.class, Errors.class, Pageable.class)
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder().title("K.Cook")
+                .description("wupitch App Back-End 연동 문서")
+                .license("© 2021. vividswan").licenseUrl("https://vividswan.github.io/").version("1").build();
+    }
+}
---
+++ src/main/java/com/server/wupitch/configure/security/SecurityConfiguration.java
@@ -0,0 +1,88 @@
+package com.server.wupitch.configure.security;
+
+import com.server.wupitch.configure.response.exception.CustomAccessDeniedHandler;
+import com.server.wupitch.configure.response.exception.CustomAuthenticationEntryPoint;
+import com.server.wupitch.configure.security.jwt.JwtAuthenticationFilter;
+import com.server.wupitch.configure.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    
+
+    @Value("${mapping.url}")
+    private String mappingUrl;
+
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins(mappingUrl)
+                    .allowedMethods("GET", "POST", "OPTIONS", "PUT");
+        }
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs/**", "/swagger-resources/**",
+                "/swagger-ui/**", "/webjars/**", "/swagger/**","/swagger.json");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/app/sign-in", "/app/sign-up").permitAll()
+                .antMatchers(HttpMethod.GET, "/errors/**", "/app/cakes/**").permitAll()
+                .antMatchers(HttpMethod.PATCH, "/app/accounts/role").hasRole("ADMIN")
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .accessDeniedHandler(new CustomAccessDeniedHandler())
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
