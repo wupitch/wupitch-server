1bc47342aac01f746dd494822fb513b61bced6b4
vividswan <vividswan@naver.com>
2021-10-19 15:53:31 +0900

    Add: Exception 관련 Class들 추가

---
+++ src/main/java/com/server/wupitch/configure/response/exception/ExceptionAdvice.java
@@ -0,0 +1,37 @@
+package com.server.wupitch.configure.response.exception;
+
+import com.server.wupitch.configure.response.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    private final ResponseService responseService;
+
+    @ExceptionHandler(CustomException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResponse customException(CustomException customException) {
+        CustomExceptionStatus status = customException.getCustomExceptionStatus();
+        log.warn(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"))+" : "+status.getMessage());
+        return responseService.getExceptionResponse(status);
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResponse customDateTimeParseException(DateTimeParseException dateTimeParseException) {
+        CustomExceptionStatus status = CustomExceptionStatus.POST_USERS_INVALID_DATE;
+        log.warn(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"))+" : "+status.getMessage());
+        return responseService.getExceptionResponse(status);
+    }
+}
---
+++ src/main/java/com/server/wupitch/configure/response/exception/ExceptionController.java
@@ -0,0 +1,32 @@
+package com.server.wupitch.configure.response.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+@ApiIgnore
+@RequiredArgsConstructor
+@RestController
+public class ExceptionController {
+
+    @GetMapping(value = "/errors/not-authenticated-account")
+    public void getNotAuthenticatedAccountException() {
+        throw new CustomException(CustomExceptionStatus.NOT_AUTHENTICATED_ACCOUNT);
+    }
+
+    @GetMapping(value = "/errors/invalid-jwt")
+    public void getInvalidJwtException() {
+        throw new CustomException(CustomExceptionStatus.INVALID_JWT);
+    }
+
+    @GetMapping(value = "/errors/empty-jwt")
+    public void getEmptyJwtException() {
+        throw new CustomException(CustomExceptionStatus.EMPTY_JWT);
+    }
+
+    @GetMapping(value = "/errors/access-Denied")
+    public void getAccessDeniedException() { throw new CustomException(CustomExceptionStatus.ACCOUNT_ACCESS_DENIED);}
+
+
+}
