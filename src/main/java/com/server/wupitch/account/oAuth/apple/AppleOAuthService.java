fbf257531fffed555ab0fcf36dfbc629e0d3e0b7
vividswan <vividswan@naver.com>
2021-10-30 20:43:25 +0900

    Add: Apple OAuth 관련 Request DTO, Service Class 생성

---
+++ src/main/java/com/server/wupitch/account/oAuth/apple/AppleOAuthService.java
@@ -0,0 +1,65 @@
+package com.server.wupitch.account.oAuth.apple;
+
+import com.server.wupitch.account.AccountRepository;
+import com.server.wupitch.account.AccountService;
+import com.server.wupitch.account.dto.AccountAuthDto;
+import com.server.wupitch.account.dto.SignInReq;
+import com.server.wupitch.account.dto.SignInRes;
+import com.server.wupitch.account.entity.Account;
+import com.server.wupitch.account.entity.enumtypes.OAuthType;
+import com.server.wupitch.configure.entity.Status;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class AppleOAuthService {
+
+    private final AccountService accountService;
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${OAuth.APPLE_TOKEN}")
+    private String APPLE_TOKEN;
+
+    public SignInRes appleLogin(AppleUserInfo appleUserInfo) {
+        String oAuthId = "apple_" + appleUserInfo.getEmail();
+
+        String nickname = appleUserInfo.getNickname();
+        String email = appleUserInfo.getEmail();
+
+        Account account = null;
+        Optional<Account> byKakaoIdAndStatus = accountRepository.findByoAuthIdAndStatus(oAuthId, Status.VALID);
+        if (byKakaoIdAndStatus.isPresent()) account = byKakaoIdAndStatus.get();
+        else {
+            AccountAuthDto accountReq = AccountAuthDto.builder()
+                    .oAuthId(oAuthId)
+                    .email(email)
+                    .nickname(nickname)
+                    .password(passwordEncoder.encode(oAuthId + APPLE_TOKEN))
+                    .build();
+
+            Account newAccount = Account.createAccount(accountReq, OAuthType.APPLE);
+            Account save = accountRepository.save(newAccount);
+            account = save;
+
+        }
+
+        SignInReq signInreq = SignInReq.builder()
+                .oAuthId(oAuthId)
+                .email(account.getEmail())
+                .password(oAuthId+APPLE_TOKEN)
+                .build();
+
+
+        return accountService.signIn(signInreq);
+
+    }
+}
---
+++ src/main/java/com/server/wupitch/account/oAuth/apple/AppleUserInfo.java
@@ -0,0 +1,13 @@
+package com.server.wupitch.account.oAuth.apple;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class AppleUserInfo {
+
+    String email;
+    String nickname;
+
+}
