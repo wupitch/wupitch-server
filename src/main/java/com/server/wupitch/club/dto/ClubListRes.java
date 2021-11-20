530f2fafc8275747ab01209d224260fce7ded72c
vividswan <vividswan@naver.com>
2021-11-21 02:05:24 +0900

    Refactor: 크루 조회 Response DTO 변경 (요일별로 시작, 종료 시간 추가)

--- src/main/java/com/server/wupitch/club/dto/ClubListRes.java
+++ src/main/java/com/server/wupitch/club/dto/ClubListRes.java
@@ -26,11 +26,51 @@ public class ClubListRes {
 
     private String clubTitle;
 
-    private List<String> timeTable = new ArrayList<>();
-
     private List<String> ageTable = new ArrayList<>();
 
     private String introduction;
+
+    private Boolean monday;
+
+    private Boolean tuesday;
+
+    private Boolean wednesday;
+
+    private Boolean thursday;
+
+    private Boolean friday;
+
+    private Boolean saturday;
+
+    private Boolean sunday;
+
+    private Double mondayStartTime;
+
+    private Double mondayEndTime;
+
+    private Double tuesdayStartTime;
+
+    private Double tuesdayEndTime;
+
+    private Double wednesdayStartTime;
+
+    private Double wednesdayEndTime;
+
+    private Double thursdayStartTime;
+
+    private Double thursdayEndTime;
+
+    private Double fridayStartTime;
+
+    private Double fridayEndTime;
+
+    private Double saturdayStartTime;
+
+    private Double saturdayEndTime;
+
+    private Double sundayStartTime;
+
+    private Double sundayEndTime;
 
     public ClubListRes(Club club) {
         this.clubId = club.getClubId();
@@ -47,57 +87,40 @@ public class ClubListRes {
 
         this.introduction = club.getIntroduction();
 
-        String startTime;
-        String endTime;
-
         if(club.getMonday() != null && club.getMonday()) {
-            if (club.getMondayStartTime() != null) startTime = club.getMondayStartTime()+":00";
-            else startTime = "00:00";
-            if(club.getMondayEndTime() != null) endTime = club.getMondayEndTime() + ":00";
-            else endTime = "24:00";
-            timeTable.add("월요일 " + startTime + " " + endTime );
+            this.monday = true;
+            this.mondayStartTime = club.getMondayStartTime();
+            this.mondayEndTime = club.getMondayEndTime();
         }
         if(club.getTuesday() != null && club.getTuesday()){
-            if (club.getTuesdayStartTime() != null) startTime = club.getTuesdayStartTime()+":00";
-            else startTime = "00:00";
-            if(club.getTuesdayEndTime() != null) endTime = club.getTuesdayEndTime() + ":00";
-            else endTime = "24:00";
-            timeTable.add("화요일 " + startTime + " " + endTime );
+            this.tuesday = true;
+            this.tuesdayStartTime = club.getTuesdayStartTime();
+            this.tuesdayEndTime = club.getTuesdayEndTime();
         }
         if(club.getWednesday() != null && club.getWednesday()){
-            if (club.getWednesdayStartTime() != null) startTime = club.getWednesdayStartTime()+":00";
-            else startTime = "00:00";
-            if(club.getWednesdayEndTime() != null) endTime = club.getWednesdayEndTime() + ":00";
-            else endTime = "24:00";
-            timeTable.add("수요일 " + startTime + " " + endTime );
+            this.wednesday = true;
+            this.wednesdayStartTime = club.getWednesdayStartTime();
+            this.wednesdayEndTime = club.getWednesdayEndTime();
         }
         if(club.getThursday() != null && club.getThursday()){
-            if (club.getThursdayStartTime() != null) startTime = club.getThursdayStartTime()+":00";
-            else startTime = "00:00";
-            if(club.getThursdayEndTime() != null) endTime = club.getThursdayEndTime() + ":00";
-            else endTime = "24:00";
-            timeTable.add("목요일 " + startTime + " " + endTime );
+            this.thursday = true;
+            this.thursdayStartTime = club.getThursdayStartTime();
+            this.thursdayEndTime = club.getThursdayEndTime();
         }
         if(club.getFriday() != null && club.getFriday()){
-            if (club.getFridayStartTime() != null) startTime = club.getFridayStartTime()+":00";
-            else startTime = "00:00";
-            if(club.getFridayEndTime() != null) endTime = club.getFridayEndTime() + ":00";
-            else endTime = "24:00";
-            timeTable.add("금요일 " + startTime + " " + endTime );
+            this.friday = true;
+            this.fridayStartTime = club.getFridayStartTime();
+            this.fridayEndTime = club.getFridayEndTime();
         }
         if(club.getSaturday() != null && club.getSaturday()){
-            if (club.getSaturdayStartTime() != null) startTime = club.getSaturdayStartTime()+":00";
-            else startTime = "00:00";
-            if(club.getSaturdayEndTime() != null) endTime = club.getSaturdayEndTime() + ":00";
-            else endTime = "24:00";
-            timeTable.add("토요일 " + startTime + " " + endTime );
+            this.saturday = true;
+            this.saturdayStartTime = club.getSaturdayStartTime();
+            this.saturdayEndTime = club.getSaturdayEndTime();
         }
         if(club.getSunday() != null && club.getSunday()){
-            if (club.getSundayStartTime() != null) startTime = club.getSundayStartTime()+":00";
-            else startTime = "00:00";
-            if(club.getSundayEndTime() != null) endTime = club.getSundayEndTime() + ":00";
-            else endTime = "24:00";
-            timeTable.add("일요일 " + startTime + " " + endTime );
+            this.sunday = true;
+            this.sundayStartTime = club.getSundayStartTime();
+            this.saturdayEndTime = club.getSaturdayEndTime();
         }
 
         if (club.getTeenager()  != null && club.getTeenager()) ageTable.add("10대");
